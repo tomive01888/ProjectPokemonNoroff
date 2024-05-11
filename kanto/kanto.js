7c6c65dfe556147561ae17a0224eaac6a7191def
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-05-11 10:09:37 +0200

    lol

--- kanto/kanto.js
+++ kanto/kanto.js
@@ -2,5 +2,5 @@ import { fetchPokemonApi } from "../javascript/export-fetch.js"
 
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
-const hoennLimiter = "?limit=151"
+const kantoLimiter = "?limit=151"
 
